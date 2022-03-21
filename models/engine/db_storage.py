898044bbea40173cde288365d270a69c27ee6b7d
Code Slayer <3613@holbertonschool.com>
2022-03-21 12:55:25 -0500

    self and close

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -95,4 +95,4 @@ class DBStorage:
 
     def close(self):
         """calls remove method on private session attribute"""
-        self.__session.remove()
+        self.__session.close()
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -67,4 +67,4 @@ class FileStorage:
 
     def close(self):
         """calls reload method for deserializing"""
-        FileStorage.reload()
+        self.reload()
